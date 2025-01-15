27bd5c5f33d4ed4479344d82cda01f019769cc5b
SSUM-47 <44286957justin@gmail.com>
2025-01-15 17:40:00 +0800

    update_sharepreferences

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'dart:convert';
 
 void main() {
   runApp(TodoApp());
 }
+
 class TodoApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -23,12 +26,39 @@ class _TodoHomePageState extends State<TodoHomePage> {
   final List<Task> _tasks = [];
   final TextEditingController _controller = TextEditingController();
 
+  @override
+  void initState() {
+    super.initState();
+    _loadTasks();  // 初始化時加載保存的任務
+  }
+
+  // 加載保存的任務
+  Future<void> _loadTasks() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String? taskListJson = prefs.getString('tasks');
+    if (taskListJson != null) {
+      List<dynamic> decodedTasks = jsonDecode(taskListJson);
+      setState(() {
+        _tasks.addAll(decodedTasks.map((task) => Task.fromJson(task)).toList());
+      });
+    }
+  }
+
+  // 儲存任務到 SharedPreferences
+  Future<void> _saveTasks() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    List<String> taskListJson =
+    _tasks.map((task) => jsonEncode(task.toJson())).toList();
+    await prefs.setString('tasks', jsonEncode(taskListJson));
+  }
+
   void _addTask(String taskName) {
     if (taskName.isNotEmpty) {
       setState(() {
         _tasks.add(Task(taskName));
       });
       _controller.clear();
+      _saveTasks();  // 保存變更
     }
   }
 
@@ -36,12 +66,14 @@ class _TodoHomePageState extends State<TodoHomePage> {
     setState(() {
       _tasks[index].isCompleted = !_tasks[index].isCompleted;
     });
+    _saveTasks();  // 保存變更
   }
 
   void _deleteTask(int index) {
     setState(() {
       _tasks.removeAt(index);
     });
+    _saveTasks();  // 保存變更
   }
 
   @override
@@ -93,7 +125,9 @@ class _TodoHomePageState extends State<TodoHomePage> {
                     children: [
                       IconButton(
                         icon: Icon(
-                          task.isCompleted ? Icons.check_box : Icons.check_box_outline_blank,
+                          task.isCompleted
+                              ? Icons.check_box
+                              : Icons.check_box_outline_blank,
                         ),
                         onPressed: () => _toggleTaskCompletion(index),
                       ),
@@ -113,9 +147,24 @@ class _TodoHomePageState extends State<TodoHomePage> {
   }
 }
 
+// 定義任務資料模型
 class Task {
   String name;
   bool isCompleted;
 
   Task(this.name, {this.isCompleted = false});
+
+  // 將 Task 轉換為 Map 格式
+  Map<String, dynamic> toJson() => {
+    'name': name,
+    'isCompleted': isCompleted,
+  };
+
+  // 從 Map 格式創建 Task 物件
+  factory Task.fromJson(Map<String, dynamic> json) {
+    return Task(
+      json['name'],
+      isCompleted: json['isCompleted'],
+    );
+  }
 }
